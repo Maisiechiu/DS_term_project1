3f579af71db6f8f6971e01d9818e72250a196972
邱敏瑄 <helencute88121@gmail.com>
2019-10-11 19:39:53 +0800

    drawmet, updatemet, detype
    寫一個updatemet 用意是判斷是不是應該要結束程式(即是否讀到End或是落下的方塊已經超過met) drawmet則是會讀取落下方塊的類型(藉由呼叫detype這個fuction)以及將會落在哪一欄  detype則會一個字元一個字元判斷 然後return一個int 讓drawmet裡的變數type接收

--- tetris.cpp
+++ tetris.cpp
@@ -36,6 +36,72 @@ void newmet(metrix* met,int row, int col){
     }
     met->index=head;
 }
+int detype (char* ch) {
+  if(ch[0]=='T'){
+    switch(ch[1]){
+    case '1': return 1; break;
+    case '2': return 2; break;
+    case '3': return 3; break;
+    case '4': return 4; break;
+    }
+  }
+  if(ch[0]=='L'){
+    switch(ch[1]){
+    case '1': return 5; break;
+    case '2': return 6; break;
+    case '3': return 7; break;
+    case '4': return 8; break;
+    }
+  }
+  if(ch[0]=='J'){
+    switch(ch[1]){
+    case '1': return 9;  break;
+    case '2': return 10; break;
+    case '3': return 11; break;
+    case '4': return 12; break;
+
+    }
+  }
+   if(ch[0]=='S'){
+    switch(ch[1]){
+    case '1': return 13;  break;
+    case '2': return 14; break;
+
+    }
+  }
+  if(ch[0]=='Z'){
+    switch(ch[1]){
+    case '1': return 15;  break;
+    case '2': return 16; break;
+
+    }
+  }
+  if(ch[0]=='I'){
+    if(ch[1]=='1')return 17;
+    else return 18;
+  }
+  if(ch[0]=='O') return 19;
+  if(ch[0]=='E') currentstate=0;
+  return 0;
+}
+
+void drawmet(metrix* met){
+    file>>ch;
+    file>>place;
+    type=detype(ch);
+    if(type==0){
+    file.close();
+     return;
+    }
+    findindex(met,type,place-1);
+}
+
+int updatemet(metrix* met){
+    do{
+    drawmet(met);
+    }while(currentstate!=0);
+    return 0;
+}
 
 
 int main ()
@@ -51,6 +117,7 @@ if(!file) {
   }
 
  newmet(met,met->row,met->col);
+ updatemet(met);
  showmet(met);
  return 0;
 }
